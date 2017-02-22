6582539b440be1e6a51e10e936de601f10795018
hf <hfloveyy@icloud.com>
2017-02-22 10:40:56 +0800

    add location to do someting2

--- app.py
+++ app.py
@@ -37,7 +37,7 @@ def wechat_auth():
         elif isinstance(recMsg, receive.Msg) and recMsg.MsgType == 'location':
             toUser = recMsg.FromUserName
             fromUser = recMsg.ToUserName
-            content = recMsg.Latitude
+            content = 'location222'
             replyMsg = reply.TextMsg(toUser, fromUser, content)
             return replyMsg.send()
         else:
